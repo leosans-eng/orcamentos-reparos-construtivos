bf737c3b40cc04e630f95cd0b74a83f051ce52dd
Leo <leosantos2127@gmail.com>
2026-07-23 09:02:04 -0300

    1.4.3

--- core/app_state.py
+++ core/app_state.py
@@ -22,7 +22,7 @@ from core.sinapi_loader import (
 )
 from core.sinapi_base import SinapiBase
 
-APP_VERSION = "1.4.2"
+APP_VERSION = "1.4.3"
 
 LARGURA_JANELA_PADRAO = 990
 ALTURA_JANELA_PADRAO = 660
